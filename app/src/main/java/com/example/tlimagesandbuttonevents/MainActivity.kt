661bb615bf0dcac0804c72e0031e3e26e9b05f2c
Tory Lawson <TORLAW1692@STUDENTS.ECPI.EDU>
2026-06-18 19:35:41 -0500

    Initial commit - Images and Button Events Android project

---
+++ app/src/main/java/com/example/tlimagesandbuttonevents/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tlimagesandbuttonevents
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+//Tory Lawson
+//GP - Images and Handling Button Events
+//06/18/2026
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val recipeButton = findViewById<Button>(R.id.btnRecipe)
+        recipeButton.setOnClickListener {
+            startActivity(Intent(this, RecipeActivity::class.java))
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
